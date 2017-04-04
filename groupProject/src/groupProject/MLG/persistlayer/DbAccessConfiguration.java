c9b9f4ff5ffe374a6bcec04663489ab7ba406310
Matt Bochenek <Matt Bochenek>
2017-04-04 15:03:38 -0400

    Changes to DBAccess

--- groupProject/src/groupProject/MLG/persistlayer/DbAccessConfiguration.java
+++ groupProject/src/groupProject/MLG/persistlayer/DbAccessConfiguration.java
@@ -8,5 +8,5 @@ public abstract class DbAccessConfiguration
 	
 	static final String DB_CONNECTION_USERNAME = "root";
 	
-	static final String DB_CONNECTION_PASSWORD = "password";
+	static final String DB_CONNECTION_PASSWORD = "krakow93";
 }
